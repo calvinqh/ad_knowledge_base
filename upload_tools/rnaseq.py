499eff4eeb6eb3e2369a5d94c2e4ce1a9c24a69e
calvinqh <calvin@blocpower.org>
2018-03-31 10:12:22 -0400

    Add comments to rnaseq py script.

--- upload_tools/rnaseq.py
+++ upload_tools/rnaseq.py
@@ -1,3 +1,14 @@
+'''
+    A script that will upload ROSMAP_RNASeq_entrez.csv onto a mongo cluster.
+    This script can be generalized for any csv file.
+
+    Instructions: 
+    User must set the server ip and port for the mongo cluster.
+    The user must also specify csv file they wish to upload.
+    The data will be uploaded in json format. 
+    Each field in the csv will act as a key.
+    Each row will become a document.
+'''
 from pymongo import MongoClient
 import csv
 import json
@@ -8,31 +19,28 @@ port = 27017
 
 #csv file configs
 csv_file_name = 'data/ROSMAP_RNASeq_entrez.csv'
-header = ["PATIENT_ID", "DIAGNOSIS"]
+header = ["PATIENT_ID", "DIAGNOSIS"] #This is temporary (the values will be initalized later)
 
 
 if __name__ == "__main__":
 
-    client = MongoClient(server_ip, port)
-    csvFile = open(csv_file_name)
-    reader = csv.DictReader( csvFile )
-    header = reader.fieldnames
-    #print(header)
-    #count = 10
+    client = MongoClient(server_ip, port) #Client used to connect to cluster
 
-    db = client.values
-    collection = db.rna
-    collection.drop()
+    csvFile = open(csv_file_name) #Load csvfile stream
+    reader = csv.DictReader( csvFile ) #initalize csv reader with file stream
+    header = reader.fieldnames #Contain the header of the csv (field names)
 
+    db = client.values  #retrieve the database Values on the cluster
+    collection = db.rna #retrieve the collection RNA from Values database
+    collection.drop() #empty the collection (drop all documents)
+
+    #For each row in the csv, convert it into json format and insert into the collection
     for each in reader:
-        '''
-        if(count < 0):
-            break
-        count-=1
-        '''
+        #create json form of row
         row = {}
         for field in header:
             row[field] = each[field]
 
+        #upload onto collection
         collection.insert(row)
 
