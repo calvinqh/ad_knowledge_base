f131d355820dca202a6bdd1dfe634bb1e4c76e97
calvinqh <calvin@blocpower.org>
2018-03-31 10:45:24 -0400

    Update cassandra upload script

--- upload_tools/patients.py
+++ upload_tools/patients.py
@@ -1,38 +1,46 @@
-from pymongo import MongoClient
+'''
+    A script that will upload patients.csv onto a cassandra cluster keyspace.
+    This script can be generalized for any csv file.
+
+    Instructions: 
+    User must set the server ip, port and keyspace for the cassandra cluster and keyspace.
+    The user must also specify csv file they wish to upload.
+    Each field in the csv will act as a column.
+    Each row will become a record.
+'''
+from cassandra.cluster import cluster
 import csv
 import json
 
 #database configs
 server_ip = 'localhost'
+keyspace = 'mylittlekeyspace'
 port = 27017
 
 #csv file configs
-csv_file_name = 'data/ROSMAP_RNASeq_entrez.csv'
-header = ["PATIENT_ID", "DIAGNOSIS"]
+csv_file_name = 'data/patients.csv'  #The file where the data is read from
+new_table_name = 'test'              #table name where the data will be inserted
+header = ["PATIENT_ID", "DIAGNOSIS"] #This is temprary (the values will be initalized later)
 
 
 if __name__ == "__main__":
 
-    client = MongoClient(server_ip, port)
-    csvFile = open(csv_file_name)
-    reader = csv.DictReader( csvFile )
-    header = reader.fieldnames
-    #print(header)
-    #count = 10
+    cluster = Cluster(server_ip)    #Cluster reference
+    session = cluster.connect(keyspace) #Session reference to specific key space
 
-    db = client.values
-    collection = db.rna
-    collection.drop()
 
-    for each in reader:
-        '''
-        if(count < 0):
-            break
-        count-=1
-        '''
-        row = {}
-        for field in header:
-            row[field] = each[field]
+    #TODO: clear the table data
+    #collection.drop()
 
-        collection.insert(row)
+    #Upload csv file into specified table name
+    session.execute("""
+        COPY %table_name FROM %csv_file_name WITH HEADER=true;
+        """,
+        {'table_name':table_name, 'csv_file_name':csv_file_name}
+    )
 
+    """
+    
+    
+
+    """
