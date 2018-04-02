1ce29181edc2a5abb5fd2b7f01f17212d0e4f0db
calvin <calvinquach@gmail.com>
2018-04-02 11:21:24 -0400

    Add comments to data py file.

--- data.py
+++ data.py
@@ -2,16 +2,22 @@ import pandas as pd
 
 from py2neo import Graph, Path, Node, Relationship
 
+'''
+    Return all genes that have a interaction with the given gene (entrez id)
+    @param gene_interactors:dataframe, the relationship between interacting genes
+    @param gene:string, the entrez id we want to find the interacting genes for
+    @return list, the list of entrez ids that interact with the given gene (entrez id) 
+'''
 def n_order_genes(gene_interactors, gene):
     graph = Graph(password = '1')
     interact = graph.begin()
-    a = gene_interactors["interactor_A"]
-    b = gene_interactors["interactor_B"]
-    n_order = []
-    i = 0
+    a = gene_interactors["interactor_A"] #Contains the series interactor A
+    b = gene_interactors["interactor_B"] #Contains the series interactor B
+    n_order = [] #Contains all genes interacting with the given gene
+    i = 0 #Index of interacting pair
 
     for i in range(a.shape[0]):
-        interactor_a = str(a[i])
+        interactor_a = str(a[i]) 
         interactor_b = str(b[i])
         if interactor_a == gene or interactor_b == gene:
             first = Node("interactor", name = interactor_a)
@@ -19,6 +25,7 @@ def n_order_genes(gene_interactors, gene):
 
             graph.merge(Relationship(first, "INTERACTS WITH", second))
 
+            #insert the gene that interacts with the given gene
             if interactor_a == gene:
                 n_order.insert(i, interactor_b)
                 i = i+1
@@ -31,6 +38,12 @@ def n_order_genes(gene_interactors, gene):
     #print("NUMBER OF GENES INTERACTING WITH {0}: {1}".format(gene, n))
     return n_order
 
+'''
+    Return the gene id associated with the given gene name
+    @param entrez_and_genes:dataframe, information about genes
+    @param gene:string, the gene we want to find the entrez id for
+    @return string, the entrez id for the given gene
+'''
 def gene_id(entrez_and_genes, gene):
     name = gene.upper()
     id = entrez_and_genes["entrez_id"]
@@ -42,6 +55,12 @@ def gene_id(entrez_and_genes, gene):
 
     return ""
 
+'''
+    Display the corresponding gene symbol for the given entrez id list (gene id)
+    @param entrez_and_genes:dataframe, information about genes (entrez_id, gene symbol ...)
+    @param g_ids:list, a list of entrez ids (gene ids) that will have their symbol displayed
+    @return None
+'''
 def gene_name(entrez_and_genes, g_ids):
     id = entrez_and_genes["entrez_id"]
     genes = entrez_and_genes["gene_symbol"]
@@ -52,6 +71,9 @@ def gene_name(entrez_and_genes, g_ids):
                 print str(genes[i])
 
     
+'''
+    CLI driver to retrieve information
+'''
 def main():
 
     print("Press [1] to find all n-order interacting genes of a GENE")
