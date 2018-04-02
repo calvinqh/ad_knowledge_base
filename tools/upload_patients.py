a4f53553d709035230141a08b65d3187cce9fa9f
calvin <calvinquach@gmail.com>
2018-04-02 13:36:34 -0400

    Update upload patient tool, add function to make code more modular.

--- tools/upload_patients.py
+++ tools/upload_patients.py
@@ -7,6 +7,10 @@
     The user must also specify csv file they wish to upload.
     Each field in the csv will act as a column.
     Each row will become a record.
+    
+    To Create a keyspace, use cqlsh:
+    CREATE KEYSPACE IF NOT EXISTS mylittlekeyspace WITH REPLICATION = 
+    {'class': 'SimpleStrategy', 'replication_factor' : 1};
 '''
 from ..models.patient import Patient 
 
@@ -18,30 +22,34 @@ import json
 
 #database configs
 server_ip = 'localhost'
-keyspace = 'mylittlekeyspace'
 port = 27017
 
-#csv file configs
-csv_file_name = 'ad_knowledge_base/data/patients.csv'  #The file where the data is read from
-header = ['patient_id', 'age', 'gender', 'education'] #This is temprary (the values will be initalized later)
-
-
-if __name__ == "__main__":
-
-
-    #Create connection to server
+'''
+    The purpose of this function is to upload patients csv to Cassandra keyspace
+    The keyspace is specified in the Patient model. (To change update patient model)
+    In this case, the keyspace is 'mylittlekeyspace'
+    @param db_ip:string, the ip of the Cass database
+    @param db_port:string, the port of the Cass database
+'''
+def upload_patient_information(db_ip, db_port='0000'):
     connection.setup([server_ip], "cqlengine", protocol_version=3)
     
     drop_table(Patient) #Drop table if it exist 
     sync_table(Patient) #In this case, it will create the table
 
-    instance = {}
+    csv_file_name = 'ad_knowledge_base/data/patients.csv'  #The file where the data is read from
+
     csvFile = open(csv_file_name) #Load csvfile stream
     reader = csv.DictReader( csvFile ) #initalize csv reader with file stream
     header = reader.fieldnames #Contains the header of the csv (field/column names)
 
     #Insert each row into database
+    instance = {}
     for row in reader:
         for field in header:
             instance[field] = row[field]
         Patient.create(**instance)
+
+
+if __name__ == "__main__":
+    upload_patient_information(server_ip)
