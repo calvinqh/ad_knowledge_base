f56367c507d2e72681cd7ade503a7bc782931713
calvin <calvinquach@gmail.com>
2018-04-06 12:59:49 -0400

    Update cli.

--- cli.py
+++ cli.py
@@ -5,14 +5,8 @@ def main():
     kb = ADKnowledgeBase(c.getMongoConfig(), c.getCassandraConfig(), c.getNeo4JConfig())
     exit = False
     while(not exit):
-        print("Enter [1] to find all n-order interacting genes of a GENE")
-        print("Enter [2] to find the mean and std of a GENEs expression values for AD/MCI/NCI")
-        print("Enter [3] to find all other information associated with a GENE")
-        print("Enter [4] to find all information of a given PATIENT")
-        print("Enter [5] to QUIT")
-        print("")
-
-        press = int(input("ENTER A NUMBER FROM 1-4: "))
+        display_menu()
+        press = int(input("ENTER A NUMBER FROM 0-10: "))
 
         if press == 1:
             input_gene = input("ENTER A GENE: ")
@@ -23,6 +17,7 @@ def main():
                 gene_num = kb.find_id(input_gene)
                 interacting_genes = kb.getNOrderGenes(gene_num)
                 print(interacting_genes)
+                print()
                 print("Enter [0] to go BACK")
                 print("Enter [1] to enter a different GENE")
                 again = int(input("Enter [0/1]   "))
@@ -34,14 +29,13 @@ def main():
                 elif again == 1:
                     input_gene = input("ENTER A GENE: ")
 
-
         elif press == 2:
             input_gene = input("ENTER A GENE: ")
             find_more = True
             while find_more:
                 gene_num = kb.find_id(input_gene)
                 print(kb.getGeneReport(gene_num))
-
+                print()
                 print("Enter [0] to go BACK")
                 print("Enter [1] to enter a different GENE")
                 again = int(input("Enter [0/1]   "))
@@ -56,6 +50,7 @@ def main():
             find_more = True
             while find_more:
                 kb.display_gene_info(input_gene)
+                print()
                 print("Enter [0] to go BACK")
                 print("Enter [1] to enter a different GENE")
                 again = int(input("Enter [0/1]   "))
@@ -71,6 +66,7 @@ def main():
             find_more = True
             while find_more:
                 print(kb.getPatientReport(input_patient_id))
+                print()
                 print("Enter [0] to go BACK")
                 print("Enter [1] to enter a different GENE")
                 again = int(input("ENTER [0/1]   "))
@@ -81,7 +77,43 @@ def main():
                 elif again == 1:
                     input_patient_id = input("ENTER PATIENT ID: ")
         elif press == 5:
+            #insert PPI
+            pass
+        elif press == 6:
+            #insert gene
+            pass
+        elif press == 7:
+            #insert patients
+            pass
+        elif press == 8:
+            #insert RosmapRna_entrez
+            pass
+        elif press == 9:
+            #insert uniprot
+            pass
+        elif press == 10:
+            #merge uniprot and gene
+            pass
+        elif press == 0:
             exit = True
+
+def display_menu():
+    print()
+    print()
+    print("===Commands Available===")
+    print("Enter [0] to QUIT")
+    print("Enter [1] to find all n-order interacting genes of a GENE")
+    print("Enter [2] to find the mean and std of a GENEs expression values for AD/MCI/NCI")
+    print("Enter [3] to find all other information associated with a GENE")
+    print("Enter [4] to find all information of a given PATIENT")
+    print("Enter [5] to UPLOAD PPI.csv")
+    print("Enter [6] to UPLOAD entrez_id_genesymbol.csv")
+    print("Enter [7] to UPLOAD patients.csv")
+    print("Enter [8] to UPLOAD ROSMAP_RNASeq_entrez.csv")
+    print("Enter [9] to UPLOAD entrez_ids_uniprot.txt")
+    print("Enter [10] to MERGE gene symbols into uniprot")
+    print("")
+
 
 
 if __name__ == "__main__":
